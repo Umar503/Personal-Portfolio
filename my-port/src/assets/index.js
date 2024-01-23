74a4b1f75abaccc0ce53bd7ae2eedc0b42f00395
Umar503 <umararshad563@gmail.com>
2024-01-23 09:16:24 +0500

    first commit

---
+++ my-port/src/assets/index.js
@@ -0,0 +1,20 @@
+export { default as Reactjs } from "./reactjs.svg";
+export { default as Nodejs } from "./nodejs.svg";
+
+export { default as Redux } from "./redux.svg";
+export { default as Express } from "./express.svg";
+export { default as Nextjs } from "./nextjs.svg";
+export { default as Nextjs2 } from "./nextjs2.svg";
+
+
+export { default as CSS3 } from "./css3.svg";
+export { default as HTML5 } from "./html.svg";
+export { default as Javascript } from "./javascript.svg";
+export { default as Python } from "./python.svg";
+
+export { default as currency } from "./work/currency.png";
+
+export { default as port1 } from "./work/port1.png";
+
+
+export { default as Profile } from "./profile.png";
---
+++ my-port/src/data.jsx
@@ -0,0 +1,111 @@
+import {
+  CSS3,
+  
+  HTML5,
+  Javascript,
+  Nextjs,
+  Nextjs2,
+  Nodejs,
+  Python,
+  Reactjs,
+  currency,
+ 
+  port1,
+ 
+} from "./assets";
+
+
+
+export const tools = [
+  {
+    name: "HTML",
+    icon: HTML5,
+  },
+  {
+    name: "CSS",
+    icon: CSS3,
+  },
+  {
+    name: "React",
+    icon: Reactjs,
+  },
+  {
+    name: "NodeJs",
+    icon: Nodejs,
+  },
+  {
+    name: "NextJs",
+    icon: Nextjs2,
+  },
+];
+
+export const skills = [
+  {
+    name: "ReactJs",
+    value: 98,
+    icon: Reactjs,
+  },
+  {
+    name: "NodeJs",
+    value: 96,
+    icon: Nodejs,
+  },
+  {
+    name: "NextJs",
+    value: 97,
+    icon: Nextjs,
+  },
+  
+  {
+    name: "CSS3",
+    value: 98,
+    icon: CSS3,
+  },
+  {
+    name: "HTML5",
+    value: 98,
+    icon: HTML5,
+  },
+  {
+    name: "JavaScript",
+    value: 50,
+    icon: Javascript,
+  },
+  {
+    name: "Python",
+    value: 70,
+    icon: Python,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Currency Converter",
+    cat: "Web Design",
+    url: "",
+    img: currency,
+  },
+  {
+    id: 2,
+    title: "Portfolio Website",
+    cat: "Web Design",
+    url: "",
+    img: port1,
+  },
+  
+  
+];
+
+export const softskills = [
+  "Time management",
+  "Communication",
+  "Empathy",
+  "Teamwork",
+  "Helpfulness",
+  "Creativity",
+  "Self-learning",
+  "Accountability",
+  "Problem solving",
+  "Open-mindedness",
+];
